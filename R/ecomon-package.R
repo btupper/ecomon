b589341cb7fc45957c9ccb9323e036885ed1d30b
Ben Tupper <btupper@bigelow.org>
2015-12-02 16:00:24 -0500

    expose ZoopRefClass

---
+++ R/ecomon-package.R
@@ -0,0 +1,7 @@
+#ecomon-package.R
+
+#' ecomon - R tools for accessing Ecomon \url{http://www.nefsc.noaa.gov/epd/ocean/MainPage/shelfwide.html} data sets.
+#'
+#' @name ecomon
+#' @docType package
+#' @imports methods sp
